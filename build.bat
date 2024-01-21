9ca28128b6ed0a92a302ca0a8fa697e5fd3325bf
zhuoy <zhuoyue_cn@yeah.net>
2024-01-21 18:36:53 +0800

    update batch file of building

--- build.bat
+++ build.bat
@@ -2,11 +2,10 @@
 set solution_dir=%cd%\src
 
 cd tools/cmm
-node dist/main.js --solution_dir %solution_dir%
+if not exist node_modules call npm install
+call node dist/main.js --solution_dir %solution_dir%
 
 cd ../..
 if not exist build mkdir build
 cd build
-cmake ../src
-
-pause()
+call cmake ../src
